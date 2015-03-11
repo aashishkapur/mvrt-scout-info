405e0a654218a81c847aea417312bcc52ec14214
Aashish Kapur <me@aashishkapur.com>
2015-03-10 23:10:11 -0700

    changed to oo

--- scripts.js
+++ scripts.js
@@ -1,23 +1,64 @@
 var eventKey = "2015cama"
 var tbaID = "115:scouting-teams:v0.2";
+var red = [];
+var blue = [];
+
 var teams = [];
+
+function Team(teamNum)
+{
+	this.teamNum = teamNum;
+}
+
+
+/*
+			<th>Team Number</th>
+			<th>Rank</th>
+			<th>Qual Avg</th>
+			<th>Auto</th>
+			<th>Container</th>
+			<th>Coop</th>
+			<th>Litter</th>
+			<th>Tote</th>
+			<th>Played</th>
+			<th>OPR</th>
+
+*/
 
 function getData(matchNum)
 {
 	$.ajax({
-		type:"POST",
+		type:"GET",
 		url: "http://www.thebluealliance.com/api/v2/event/" + eventKey + "/matches",
 		data: {"X-TBA-App-Id": tbaID},
 		dataType: "json",
+		async: false,
 		success: function(data){
 			for(var i = 0; i < data.length; i++)
 			{
-				console.log(data[i]);
+				if(data[i]["comp_level"] == "qm" && data[i]["match_number"] == matchNum)
+				{
+					console.log(data[i]);
+					blue[0] = data[i]["alliances"]["blue"]["teams"][0].substring(3);
+					blue[1] = data[i]["alliances"]["blue"]["teams"][1].substring(3);
+					blue[2] = data[i]["alliances"]["blue"]["teams"][2].substring(3);
+
+					red[0] = data[i]["alliances"]["red"]["teams"][0].substring(3);
+					red[1] = data[i]["alliances"]["red"]["teams"][1].substring(3);
+					red[2] = data[i]["alliances"]["red"]["teams"][2].substring(3);
+				}
 			}
 
 		}
 	});
 }
+
+function getStats()
+{
+
+}
+
+
 
 $(document).ready(function() {
 
@@ -26,7 +67,10 @@ $(document).ready(function() {
 		if($("#matchNum").val() === "")
 			alert("please enter the match number");
 		else
+		{
 			getData(parseInt($("#matchNum").val()));
+			getStats();
+		}
 	});
 
 
